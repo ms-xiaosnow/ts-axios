b836c6d5d8a3dad8cfa99bb12fc37d4f72086065
ms-xiaosnow <1103184133@qq.com>
2020-02-06 19:52:00 +0800

    05-05 处理请求 body 数据 - 需求分析+ 实现

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,6 +1,6 @@
 import axios from '../../src/index'
 
-axios({
+/*axios({
   method: 'get',
   url: '/base/get',
   params: {
@@ -59,38 +59,39 @@ axios({
   params: {
     bar: 'baz'
   }
+})*/
+
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
 })
 
+const arr = new Int32Array([21, 31])
 
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
+
+/*axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})*/
 //
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// })
-//
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   headers: {
-//     'content-type': 'application/json;charset=utf-8'
-//   },
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// })
-//
-// const arr = new Int32Array([21, 31])
-//
-// axios({
-//   method: 'post',
-//   url: '/base/buffer',
-//   data: arr
-// })
+
 //
 //
 // const paramsString = 'q=URLUtils.searchParams&topic=api'
