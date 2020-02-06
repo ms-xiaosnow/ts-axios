f18f2e78e7019b26518dbb1b834a6eca37045f68
ms-xiaosnow <1103184133@qq.com>
2020-02-06 22:23:56 +0800

    05-08 处理请求 header - 实现请求 header 处理逻辑 05-09 处理请求 header - demo 编写

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -61,47 +61,48 @@ axios({
   }
 })*/
 
-
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: {
-    a: 1,
-    b: 2
-  }
-})
-
-const arr = new Int32Array([21, 31])
+/*const arr = new Int32Array([21, 31])
 
 axios({
   method: 'post',
   url: '/base/buffer',
   data: arr
+})*/
+
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
 })
 
-/*axios({
+axios({
   method: 'post',
   url: '/base/post',
   headers: {
-    'content-type': 'application/json;charset=utf-8'
+    'content-type': 'application/json;charset=utf-8',
+    'Accept': 'application/json, text/plain, */*'
   },
   data: {
     a: 1,
     b: 2
   }
-})*/
-//
+})
 
-//
-//
-// const paramsString = 'q=URLUtils.searchParams&topic=api'
-// const searchParams = new URLSearchParams(paramsString)
-//
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: searchParams
-// })
+
+
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
 
 /*
 axios({
