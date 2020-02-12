673ffb3e5970f8697b3b8ccf7cba899a7b80105f
ms-xiaosnow <1103184133@qq.com>
2020-02-12 21:33:32 +0800

    11-10 baseURL 需求分析+ 代码编写 + demo编写

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -115,6 +115,7 @@ axios.get('/more/304', {
   console.log(e.message)
 })
 */
+/*
 axios.get('/more/get', {
   params: new URLSearchParams('a=b&c=d')
 }).then(res => {
@@ -146,4 +147,13 @@ instance.get('/more/get', {
 }).then(res => {
   console.log(res)
 })
+*/
 
+const instance = axios.create({
+  baseURL: 'https://img.mukewang.com/'
+})
+
+instance.get('5cc01a7b0001a33718720632.jpg')
+
+instance.get('https://img.mukewang.com/szimg/' +
+  '5becd5ad0001b89306000338-360-202.jpg')
--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -65,6 +65,14 @@ export function buildURL(url: string, params?: any, paramsSerializer?: (params: 
   return url
 }
 
+export function isAbsoluteUrl(url: string): boolean {
+  return /(^[a-z][a-z\d\+\-\.]*:)?\/\//i.test(url)
+}
+
+export function combineURL(baseURL: string, relativeURL?: string): string {
+  return relativeURL ? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '') : baseURL
+}
+
 export function isURLSameOrigin(requestURL: string): boolean {
   const parsedOrigin = resolveURL(requestURL)
   return (parsedOrigin.protocol === currentOrigin.protocol && parsedOrigin.host === currentOrigin.host)
