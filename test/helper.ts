5335516564956e816a421dd5009f1904b792996a
ms-xiaosnow <1103184133@qq.com>
2020-02-15 01:08:09 +0800

    12-8 请求模块单元测试 jasmine-ajax + 测试代码编写

---
+++ test/helper.ts
@@ -0,0 +1,8 @@
+export function getAjaxRequest(): Promise<JasmineAjaxRequest> {
+  return new Promise(function(resolve) {
+    setTimeout(() => {
+      return resolve(jasmine.Ajax.requests.mostRecent())
+    }, 0)
+  })
+
+}
